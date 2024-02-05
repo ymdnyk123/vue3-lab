0267b25e5eaf915effbdd4c3107e66b0f908ce83
山田尚征 <ymdnyk123@gamil.com>
2024-02-06 00:14:43 +0900

    今日の進捗。商品を1個レンダリング。

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,111 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+const item = ref({
+  id: 1,
+  name: 'アボガドディップバケット',
+  description: '刻んだ野菜をアボガドと混ぜてディップに。こんがり焼いたバゲットとお召し上がりください。',
+  price: '400',
+  image: '/images/item1.jpg'
+})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="I'm Naoyuki Yamada!!!" />
-    </div>
+  <header class="header">
+    <img 
+      src="/images/logo.svg"
+      alt="">
+    <h1>Vue.js ハンズオン</h1>
   </header>
 
-  <main>
-    <TheWelcome />
+  <main class="main">
+    <div class="item">
+      <div class="thumbnail">
+        <img
+          :src="item.image"
+          alt="">
+      </div>
+      <div class="description">
+        <h2>{{ item.name }}</h2>
+        <p>{{ item.description }}</p>
+        <span>¥<span class="price">{{ item.price }}</span></span>
+      </div>
+    </div>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+body {
+  font-family: sans-serif;
+  margin: 0;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 }
 
-.logo {
+#app {
+  width: 90%;
+  margin: 0 5%;
+  color: #242424;
+}
+
+.header {
+  display: flex;
+  align-content: center;
+  align-items: center;
+  margin-top: 40px;
+  margin-bottom: 40px;
+}
+
+.header > h1 {
+  font-size: 80px;
+  font-weight: bold;
+  line-height: 80px;
+  margin-top: 0;
+  margin-bottom: 0;
+}
+
+.main {
+  display: grid;
+  grid-template-columns: 3fr 3fr 3fr 3fr;
+  column-gap: 24px;
+  row-gap: 24px;
+}
+
+.item {
+  padding: 10px;
+  cursor: pointer;
+}
+
+.item:hover {
+  transition: 0.2s transform ease-out;
+  transform: scale(1.05);
+}
+
+.item > div.thumbnail > img {
+  width: 100%;
+  height: calc(100%);
+  object-fit: cover;
+}
+
+.item > div.description {
+  text-align: left;
+  margin-top: 20px;
+}
+
+.item > div.description > p {
+  margin-top: 0px;
+  margin-bottom: 0px;
+  font-size: 18px;
+  line-height: 25px;
+}
+
+.item > div.description > span {
   display: block;
-  margin: 0 auto 2rem;
+  margin-top: 10px;
+  font-size: 20px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.item > div.description > span > .price {
+  font-size: 28px;
+  font-weight: bold;
 }
 </style>
